74f7cde7e5465e07a4443a6b5c382ebef41f46a8
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-02-03 22:40:46 -0300

    exercicio para verificar idade

---
+++ material didático/curso em vídeo/EXERCÍCIOS/02-idade/script.js
@@ -0,0 +1,9 @@
+function calcular(){
+    var agora = new Date()
+    var ano = agora.getFullYear ()  // recebe o ano configurado no sistema operacional
+    var msg = window.document.getElementById('idade')
+    var img = window.document.getElementById('imagem')
+    
+   
+
+}
