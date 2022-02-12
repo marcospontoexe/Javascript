8d5139c47c2070f55486ff62fc78488819d26760
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-02-11 23:30:10 -0300

    Update script.js

--- material didático/curso em vídeo/EXERCÍCIOS/03-contador/script.js
+++ material didático/curso em vídeo/EXERCÍCIOS/03-contador/script.js
@@ -1,14 +1,19 @@
 function contador(){
-    var inicio = Number(window.document.getElementById('inicio').value)
-    var fim = Number(window.document.getElementById('fim').value)
-    var contador = Number(window.document.getElementById('passo').value)
+    var inicio = window.document.getElementById('inicio')
+    var fim = window.document.getElementById('fim')
+    var contador = window.document.getElementById('passo')
     var msg = window.document.getElementById('resposta')
 
-    msg.innerHTML = 'Contando:'
-    for(var c = inicio; c <= fim; c+=contador){
-        msg.innerHTML += `${c}  `
-    }   
+
+    if(fim.value.length == 0 || ){   
+        msg.innerHTML += 'Contando:'
+        for(var c = Number(inicio.value); c <= Number(fim.value); c+=Number(contador.value)){
+            msg.innerHTML += `${c}  `
+        }
+    } 
+
     
-   
+    
+
 
 }
